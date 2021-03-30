0dd6bc91e481f034603de6951cadeb6a0ec2bff0
austinjdowney <austinjdowney@gmail.com>
2021-03-30 14:36:41 -0600

    morning challenge

---
+++ app.js
@@ -0,0 +1,15 @@
+let str = "What's the longest word in this sentence"
+
+
+function longestWord(str) {
+    let arra = text.splt(' ')
+    let longest = ' '
+
+    for (let i = 0; arr.length > i; i++) {
+        let word = arr[i]
+        if (word.length[i] > longest.length) {
+            longest = word
+        }
+    }
+    return longest
+}
